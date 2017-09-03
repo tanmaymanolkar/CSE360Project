cca85eebe797e5c5ec90fb36159fe632f1b52c16
Alias44 <25497382+Alias44@users.noreply.github.com>
2017-09-03 16:53:06 -0700

    Update Tutor.java

--- Part 1/Tutor.java
+++ Part 1/Tutor.java
@@ -1,6 +1,5 @@
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import javax.swing.*;
 
@@ -12,17 +11,42 @@ public class Tutor extends JPanel {
     Tutor() {
 		panel= new JPanel();
 
-		Tutor(1);
+		Tutor();
 
         setLayout(new BorderLayout());
 		add(panel);
     }
 
-    public void Tutor(int state) {
+	public void Tutor() {
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setEditable(false);
 
-        if (state >= 1 || state <= 4) {
-           JEditorPane editorPane = new JEditorPane();
-           editorPane.setEditable(false);
+		File file = new File("P1.html");
+		java.net.URL url;
+
+		try {
+			url = file.toURI().toURL();
+		}
+		catch (MalformedURLException e) {
+			url = null;
+			System.err.println("Error: Attempted to read bad URL");
+		}
+
+
+		try
+		{
+			editorPane.setPage("http://www.google.com");
+		}
+		catch (IOException e)
+		{
+			editorPane.setContentType("text/html");
+			editorPane.setText("<html>Could not load http://www.google.com </html>");
+		}
+		 JScrollPane scrollPane = new JScrollPane(editorPane);
+		 panel.add(scrollPane);
+
+       /* if (state >= 1 || state <= 4) {
+
 
 
 
@@ -59,6 +83,7 @@ public class Tutor extends JPanel {
            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
            scrollPane.setPreferredSize(new Dimension(250,145));
            scrollPane.setMinimumSize(new Dimension(10,10));
-        }
+        }*/
     }
+
 }
