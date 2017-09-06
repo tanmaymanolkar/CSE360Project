7aa29b70d6dd22a3aca68a12eaaff27bbd43fb1a
swgarverick <31574942+swgarverick@users.noreply.github.com>
2017-09-06 15:50:47 -0700

    Update Universe.java

--- Part 1/Universe.java
+++ Part 1/Universe.java
@@ -10,16 +10,16 @@ public class Universe extends JFrame{
     JPanel BL = new JPanel();
     JPanel BR = new JPanel();
 
-    Companion comp = new Companion(new Color(0,105,145));
+    Companion comp = new Companion();
     Assessor assess = new Assessor();
-	Tutor tutor = new Tutor();
+    Tutor tut = new Tutor();
 
 	private JLabel Steven = new JLabel("Steven Garverick");
 
 	public static void main(String[] args) {	//DRIVER
 		Universe x = new Universe();
 	}
-
+	
 	public Universe(){
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	//Program exits when frame closes
@@ -31,16 +31,19 @@ public class Universe extends JFrame{
 
         add(container, BorderLayout.CENTER);//Put Panel in Center position on frame
         container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS)); // Box layout on the y axis
-
+        container.setBackground(new Color(255,198,39));
+        
         JSlider slider = new JSlider(JSlider.HORIZONTAL, 1, 4, 1); //create slider
         slider.setMajorTickSpacing(1);
         slider.setPaintTicks(true);
         slider.addChangeListener(new SliderListener()); //listen to the slider
+        slider.setBackground(new Color(255,198,39));
 
         JPanel TopHalf = new JPanel();	//Panel to hold other panels up top
-        TopHalf.setLayout(new GridLayout(2,2));	//2X2 grid layout
+        TopHalf.setLayout(new GridLayout(2,2,10,10));	//2X2 grid layout
+        TopHalf.setBackground(new Color(255,198,39));
 
-	// Set font sizes
+		// Set font sizes
         Steven.setFont(Steven.getFont().deriveFont(32f));
 
         //set panel colors
@@ -48,17 +51,17 @@ public class Universe extends JFrame{
         TR.setBackground(new Color(0,105,145));
         BL.setBackground(new Color(0,105,145));
         BR.setBackground(new Color(0,105,145));
-
+        
         //ADD objects to corresponding spots on grid and set borders
         TL.add(comp, BorderLayout.CENTER);
         TL.setBorder(BorderFactory.createLineBorder(Color.black));
-        TR.add(tutor, BorderLayout.CENTER);
+        TR.add(tut, BorderLayout.CENTER);
         TR.setBorder(BorderFactory.createLineBorder(Color.black));
         BL.add(assess, BorderLayout.CENTER);
         BL.setBorder(BorderFactory.createLineBorder(Color.black));
         BR.add(Steven, BorderLayout.CENTER);
         BR.setBorder(BorderFactory.createLineBorder(Color.black));
-
+        
         //add panels to grid
         TopHalf.add(TL);
         TopHalf.add(TR);
@@ -74,38 +77,38 @@ public class Universe extends JFrame{
 	public void position1() {
 		Steven.setVisible(false);
 		assess.phase1();
-		comp.happy();
-		tutor.phase1();
-		refresh();
+		tut.phase1();
+		comp.Happy();
+		invalidate();
+		validate();
+		repaint();
 	}
 
 	public void position2() {
 		Steven.setVisible(false);
 		assess.phase2();
-		comp.thinking();
-		tutor.phase2();
-		refresh();
+		tut.phase2();
+		comp.Thinking();
+		invalidate();
+		validate();
+		repaint();
 	}
-
+	
 	public void position3() {
 		Steven.setVisible(false);
 		assess.phase3();
-		comp.worry();
-		tutor.phase3();
-		refresh();
+		tut.phase3();
+		comp.Worry();
+		invalidate();
+		validate();
+		repaint();
 	}
 
 	public void position4() {
 		Steven.setVisible(false);
 		assess.phase4();
-		comp.sorry();
-		tutor.phase4();
-		refresh();
-	}
-
-	private void refresh()
-	{
-		comp.size(TL.getSize().height -50, TL.getSize().width -50);
+		tut.phase4();
+		comp.Sorry();
 		invalidate();
 		validate();
 		repaint();
