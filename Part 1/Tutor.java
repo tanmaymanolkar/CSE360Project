323c13c296379c4a856b5e052e7d8d06e60d5315
KaitlynDiLorenzo <31551098+KaitlynDiLorenzo@users.noreply.github.com>
2017-09-07 16:38:11 -0700

    Add files via upload

--- Part 1/Tutor.java
+++ Part 1/Tutor.java
@@ -1,3 +1,11 @@
+//**************************************************************
+// AUTHOR: Kaitlyn DiLorenzo
+// STUDENT ID: 1208983772
+// FOR: CSE 360
+// SPECIFICATION: Opens HTML files in a JPanel
+// DUE DATE: 9/8/2017
+//**************************************************************
+
 import java.awt.*;
 import java.io.*;
 import java.net.MalformedURLException;
@@ -11,37 +19,48 @@ public class Tutor extends JPanel {
     private static JPanel panel;
     private static JScrollPane scrollPane;
     private static final JLabel NAME_LABEL = new JLabel("Kaitlyn DiLorenzo");
-    private static final JLabel ERROR_LABEL = new JLabel("Error");
+    private static final JLabel ERROR_LABEL = new JLabel("Error: Could not open file");
 
     public  Tutor() {
+        
+        //Create new JEditorPane, JPanel, and JScrollPane objects
         editorPane = new JEditorPane();
         panel = new JPanel();
         scrollPane = new JScrollPane(editorPane);
         
         editorPane.setEditable(true);
         
-        scrollPane.setPreferredSize(new Dimension(600, 450));
+        //Set JScrollPane settings
+        scrollPane.setPreferredSize(new Dimension(600, 370));
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         
+        //Set JLabel settings
         NAME_LABEL.setFont(NAME_LABEL.getFont().deriveFont(32f));
+        NAME_LABEL.setBackground(new Color(0, 105, 145));
+        NAME_LABEL.setOpaque(true);
+        
+        ERROR_LABEL.setFont(NAME_LABEL.getFont().deriveFont(32f));
+        ERROR_LABEL.setBackground(new Color(0, 105, 145));
+        ERROR_LABEL.setOpaque(true);
+        
+        //Set panel color and add JLabel
+        panel.setBackground(new Color(0, 105, 145));
+        panel.setOpaque(true);
         panel.add(NAME_LABEL);
 
         setLayout(new BorderLayout());
 	add(panel);
     }
     
+    //Open HTML file
     private void setHTML(String fileName) {
-        file = new File(fileName);
+        file = new File("resources/" + fileName);
 
 	try {
             url = file.toURI().toURL();
 	}
 	catch(MalformedURLException e) {
-            url = null;
-            
-            ERROR_LABEL.setText("Error: Could not open file");
-            scrollPane.add(ERROR_LABEL);
-            
+            error();
             return;
 	}
         
@@ -49,11 +68,7 @@ public class Tutor extends JPanel {
             editorPane.setPage(url);
         }
         catch(IOException e) {
-            url = null;
-            
-            ERROR_LABEL.setText("Error: Could not open file");
-            scrollPane.add(ERROR_LABEL);
-            
+            error();
             return;
         }
         
@@ -64,23 +79,38 @@ public class Tutor extends JPanel {
         url = null;
     }
     
+    //Display error message
+    private void error() {
+        System.out.println("Entered error()");
+        
+        url = null;
+        
+        panel.removeAll();
+        panel.add(ERROR_LABEL);
+    }
+    
+    //Display P1.html
     public void phase1() {
         panel.removeAll();
         setHTML("P1.html");
     }
     
+    //Display P2.html
     public void phase2() {
         panel.removeAll();
         setHTML("P2.html");
     }
     
+    //Display P3.html
     public void phase3() {
         panel.removeAll();
         setHTML("P3.html");
     }
     
+    //Display P4.html
     public void phase4() {
         panel.removeAll();
         setHTML("P4.html");
     }
 }
+
