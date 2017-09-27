1a354f806652eadb1564cc02c452e7f1575dc1c9
Alias44 <25497382+Alias44@users.noreply.github.com>
2017-09-27 15:40:00 -0700

    Trimmed White Space

--- Part 2/project/src/Assessor.java
+++ Part 2/project/src/Assessor.java
@@ -4,7 +4,7 @@
 //FOR CSE 360
 //SPECIFICATION:This is Assessor Class. 
 //It creates various UI input elements and provides some output.
-//version: 1.0
+//version: 1.0.1
 //DUE DATA : 9/8/2017
 //------------------------------------------------------
 import java.awt.*;
@@ -13,52 +13,28 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Assessor extends JPanel{
-
-
-
+	
+	//declarations
 	public JPanel p1;
-
 	public JButton b1,b2,b3,b4;
-
-	public JTextField t1;					//declarations
-
-	public JCheckBox[] cb1;
-	
+	public JTextField t1;					
+	public JCheckBox[] cb1;	
 	public JFrame f2;
-
 	
-
-
-
 	
-
-	
-
 	public Assessor(){
-
 		createPanel();					//constructor
-
 	}
 
-	
-
-	public void createPanel() {
-
-		
-
-		
+	public void createPanel() {	
 		JLabel name = new JLabel("Tanmay Manolkar");			
 		name.setFont(name.getFont().deriveFont(32f));			//Constructor calls this function
-        add(name, BorderLayout.CENTER);							//initial display created 
+        	add(name, BorderLayout.CENTER);							//initial display created 
 		setLayout(new GridLayout(4,3));
 		setBackground(new Color(0,0,0,0));
-
-		
-
 	}
 	
-	public void phase3() {
-		
+	public void phase3() {		
 		removeAll();
 		b1 = new JButton("option 1");
 		b2 = new JButton("option 2");						//creation of buttons with action listener connected
@@ -69,10 +45,7 @@ public class Assessor extends JPanel{
 
 		add(b1);
 		add(b2);
-		add(b3);
-		
-
-		
+		add(b3);		
 	}
    
 	public void phase4() {
@@ -85,11 +58,6 @@ public class Assessor extends JPanel{
 		add(l1);
 		add(t1);
 		add(b4);
-
-		
-
-		
-		
 	}
 	
 	public void phase1()
@@ -99,9 +67,7 @@ public class Assessor extends JPanel{
 
 		JComboBox<String> cb2 = new JComboBox<String>(str1);
 		cb2.addActionListener(new buttonPressed());												
-		add(cb2);
-		
-		
+		add(cb2);	
 	}
 	
 	public void phase2() {
@@ -109,43 +75,31 @@ public class Assessor extends JPanel{
 		cb1 = new JCheckBox[3];
 
 		for (int y=0;y<3;y++) {				// creation of check boxes with action listener with action listener connected
-
 			cb1[y] = new JCheckBox();
-
 			cb1[y].setText("option" + y);
-
 			cb1[y].addActionListener(new buttonPressed());	
 			add(cb1[y]);
 		
-	}
+		}
 		
 	}
+	
 	class buttonPressed implements ActionListener{
-
 		 public void actionPerformed(ActionEvent e) {			//Action listener defined
-
 			 JOptionPane.showMessageDialog(f2, "Hello World!");
 				t1.setText("");
-
-
 		}
 
 	}
 
 
-	Action act1 = new AbstractAction() {
-										//Showing message after enter is pressed in text field
+	Action act1 = new AbstractAction() {									//Showing message after enter is pressed in text field
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			JOptionPane.showMessageDialog(f2, "Hello World!");
 			t1.setText("");			
 		}
 	};
-
-
-
-
-
 }
 
 
